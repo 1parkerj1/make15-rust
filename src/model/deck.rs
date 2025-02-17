627440673c0f379d3c39258e81325c8b6e926053
parker <1parkerj1@gmail.com>
2025-02-17 23:17:13 +0000

    created deck file and started deck struct this is hard Zzz

---
+++ src/model/deck.rs
@@ -0,0 +1,23 @@
+use crate::Card;
+
+pub struct Deck {
+    
+    // add globals for ranks and suits
+    // ["Clubs", "Diamonds", "Hearts", "Spades"]
+    // ["2","3","4","5","6", "7","8","9","10", "Jack","Queen","King","Ace"]
+
+    deck_cards: Vec<Card>,
+}
+
+impl Deck {
+    // not sure how to do this either
+    // need to make constructor that checks deck num
+    // creates a new vector of cards using a create_deck function
+    // and shuffles the deck :)
+    pub fn new (deck_num: i32,) -> Self {
+        
+    }
+}
+
+
+
